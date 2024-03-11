acc3e600de989a30d105c6f81afd883309d1e31c
Bryan Simbaña <ba.simbanaa@uea.edu.ec>
2024-03-10 20:17:27 -0500

    3D

---
+++ Covertidor temperatura/Matriz 3D.py
@@ -0,0 +1,137 @@
+# Crear una matriz 3D para almacenar datos de temperaturas
+# Primera dimensión: Ciudades (3 ciudades)
+# Segunda dimensión: Semanas (4 semanas)
+# Tercera dimensión: Días de la semana (7 días)
+temperaturas = [
+    [   # guayaquil
+        [   # Semana 1
+            {"day": "Lunes", "temp": 30},
+            {"day": "Martes", "temp": 34},
+            {"day": "Miércoles", "temp": 29},
+            {"day": "Jueves", "temp": 28},
+            {"day": "Viernes", "temp": 34},
+            {"day": "Sábado", "temp": 36},
+            {"day": "Domingo", "temp": 24}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 30},
+            {"day": "Martes", "temp": 19},
+            {"day": "Miércoles", "temp": 28},
+            {"day": "Jueves", "temp": 32},
+            {"day": "Viernes", "temp": 17},
+            {"day": "Sábado", "temp": 31},
+            {"day": "Domingo", "temp": 30}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 24},
+            {"day": "Martes", "temp": 31},
+            {"day": "Miércoles", "temp": 25},
+            {"day": "Jueves", "temp": 28},
+            {"day": "Viernes", "temp": 18},
+            {"day": "Sábado", "temp": 32},
+            {"day": "Domingo", "temp": 15}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 14},
+            {"day": "Martes", "temp": 18},
+            {"day": "Miércoles", "temp": 28},
+            {"day": "Jueves", "temp": 19},
+            {"day": "Viernes", "temp": 23},
+            {"day": "Sábado", "temp": 27},
+            {"day": "Domingo", "temp": 31}
+        ]
+    ],
+    [   # Quito
+        [   # Semana 1
+            {"day": "Lunes", "temp": 12},
+            {"day": "Martes", "temp": 14},
+            {"day": "Miércoles", "temp": 18},
+            {"day": "Jueves", "temp": 20},
+            {"day": "Viernes", "temp": 13},
+            {"day": "Sábado", "temp": 16},
+            {"day": "Domingo", "temp": 19}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 13},
+            {"day": "Martes", "temp": 16},
+            {"day": "Miércoles", "temp": 10},
+            {"day": "Jueves", "temp": 12},
+            {"day": "Viernes", "temp": 15},
+            {"day": "Sábado", "temp": 16},
+            {"day": "Domingo", "temp": 21}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 16},
+            {"day": "Martes", "temp": 15},
+            {"day": "Miércoles", "temp": 18},
+            {"day": "Jueves", "temp": 20},
+            {"day": "Viernes", "temp": 12},
+            {"day": "Sábado", "temp": 10},
+            {"day": "Domingo", "temp": 20}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 14},
+            {"day": "Martes", "temp": 17},
+            {"day": "Miércoles", "temp": 19},
+            {"day": "Jueves", "temp": 21},
+            {"day": "Viernes", "temp": 24},
+            {"day": "Sábado", "temp": 14},
+            {"day": "Domingo", "temp": 21}
+        ]
+    ],
+    [   # La Joya de los Sachas
+        [   # Semana 1
+            {"day": "Lunes", "temp": 32},
+            {"day": "Martes", "temp": 33},
+            {"day": "Miércoles", "temp": 24},
+            {"day": "Jueves", "temp": 31},
+            {"day": "Viernes", "temp": 28},
+            {"day": "Sábado", "temp": 19},
+            {"day": "Domingo", "temp": 32}
+        ],
+        [   # Semana 2
+            {"day": "Lunes", "temp": 29},
+            {"day": "Martes", "temp": 32},
+            {"day": "Miércoles", "temp": 33},
+            {"day": "Jueves", "temp": 30},
+            {"day": "Viernes", "temp": 27},
+            {"day": "Sábado", "temp": 28},
+            {"day": "Domingo", "temp": 24}
+        ],
+        [   # Semana 3
+            {"day": "Lunes", "temp": 29},
+            {"day": "Martes", "temp": 33},
+            {"day": "Miércoles", "temp": 25},
+            {"day": "Jueves", "temp": 32},
+            {"day": "Viernes", "temp": 29},
+            {"day": "Sábado", "temp": 26},
+            {"day": "Domingo", "temp": 23}
+        ],
+        [   # Semana 4
+            {"day": "Lunes", "temp": 18},
+            {"day": "Martes", "temp": 20},
+            {"day": "Miércoles", "temp": 32},
+            {"day": "Jueves", "temp": 19},
+            {"day": "Viernes", "temp": 26},
+            {"day": "Sábado", "temp": 28},
+            {"day": "Domingo", "temp": 30}
+        ]
+    ]
+]
+# Calcular el promedio de temperaturas para cada ciudad y semana
+no_ciudad = 0
+for ciudad in temperaturas:
+    no_ciudad = no_ciudad + 1
+    print(f'CIUDAD No. {no_ciudad}')
+    no_semana = 0
+    suma_promedio = 0
+    for semana in ciudad:
+        no_semana += 1
+        suma = 0
+        for dia in semana:
+            suma = suma + dia['temp']
+        promedio = round(suma / len(semana), 1)
+        suma_promedio += promedio
+        print(f'El promedio semana No. {no_semana} es: {promedio}')
+    promedio_ciudad = round(suma_promedio / len(ciudad), 1)
+    print(f'El promedio mensual es: {promedio_ciudad}')
